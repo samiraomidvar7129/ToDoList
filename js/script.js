bd9c0a0c1b3f5d7b0425e1b425392a83558344f0
samiraomidvar7129 <yasaminomid9643@gmail.com>
2022-06-30 19:42:05 +0430

    upload ToDoList project on github

---
+++ js/script.js
@@ -0,0 +1,46 @@
+﻿let form=document.querySelector('.add')
+let InputElem=document.querySelector('input')
+let UlList=document.querySelector('ul')
+let newItem=document.createElement('li')
+
+
+
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    })
+
+
+function addnewToDo(newToDoValue){
+    let newliElement=document.createElement('li')
+    newliElement.className='list-group-item d-flex justify-content-between align-items-center'
+
+    
+    let span=document.createElement('span')
+    span.innerHTML=newToDoValue
+
+    let newTrash=document.createElement('i')
+    newTrash.className='fa fa-trash-o delete'
+
+    newTrash.addEventListener("click",function(event){
+        event.target.parentElement.remove()
+     
+    });
+   
+     
+    newliElement.append(span,newTrash)
+    UlList.append(newliElement)
+//   console.log(newliElement)
+
+};
+
+
+InputElem.addEventListener('keydown',function(event){
+let newToDoValue=event.target.value.trim()
+if(event.keyCode === 13){
+    if (newToDoValue) {
+        InputElem.value=''
+       addnewToDo(newToDoValue)
+    }
+} 
+});
